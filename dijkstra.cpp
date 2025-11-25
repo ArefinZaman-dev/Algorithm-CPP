8d357a5e773f05012b2dc2e8eee0b9c203f24f86
Shaikh Arefin Zaman <shaikhzaman953@gmail.com>
2025-11-25 18:32:02 +0600

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,28 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> graph[20], cost[20];
-int dis[20], parent[20];
+vector<int> graph[100], cost[100];
+int dis[100], parent[100];
 
 void dijkstra(int source, int n)
 {
-    priority_queue<pair<int, int> > pq;
+    priority_queue<pair<int, int>> pq; 
+
     for (int i = 0; i < n; i++) {
         dis[i] = INT_MAX;
         parent[i] = -1;
     }
+
     dis[source] = 0;
     pq.push({-dis[source], source});
+
     while (!pq.empty()) {
-        pair<int, int> node;
-        node = pq.top();
+        int u = pq.top().second;
         pq.pop();
-        int u = node.second;
+
         for (int i = 0; i < graph[u].size(); i++) {
             int v = graph[u][i];
             int w = cost[u][i];
 
-            // Relaxation
+            // Relaxation step
             if (dis[u] + w < dis[v]) {
                 dis[v] = dis[u] + w;
                 parent[v] = u;
@@ -32,21 +34,56 @@ void dijkstra(int source, int n)
     }
 }
 
+void printPath(int src, int des)
+{
+    if (des == -1) return;
+    if (src == des) {
+        cout << src << " ";
+        return;
+    }
+    printPath(src, parent[des]);
+    cout << des << " ";
+}
+
 int main()
 {
-    int n, m, u, v, w;
-    cout << "Enter the no. of vertex and edges : ";
-    cin >> n >> m;          // n = no of node, m = no of edge
-    cout << "Enter u v w for each edge : " << endl;
+    int n, m;
+    cout << "Enter number of vertices: ";
+    cin >> n;
+
+    cout << "Enter number of edges: ";
+    cin >> m;
+
+    cout << "Enter edges (u v w):" << endl;
     for (int i = 0; i < m; i++) {
+        int u, v, w;
         cin >> u >> v >> w;
+
+        // Directed graph
         graph[u].push_back(v);
         cost[u].push_back(w);
     }
-    dijkstra(0, n);
-    for (int i = 0; i < n; i++) {
-        cout << dis[i] << " ";
+
+    int source, destination;
+    cout << "Enter source node: ";
+    cin >> source;
+
+    cout << "Enter destination node: ";
+    cin >> destination;
+
+    dijkstra(source, n);
+
+    if (dis[destination] == INT_MAX) {
+        cout << "No path exists!" << endl;
+        return 0;
     }
+
+    cout << "Shortest distance from " << source << " to " << destination 
+         << " = " << dis[destination] << endl;
+
+    cout << "Path: ";
+    printPath(source, destination);
     cout << endl;
+
     return 0;
 }
